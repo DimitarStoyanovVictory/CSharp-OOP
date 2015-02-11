dfa7fa0e844641932cbe32a34062feb0aeb14611
DimitarStoyanovVictory <Dimitar.Stoyanov.Victory@gmail.com>
2015-02-11 15:48:45 +0200

    ch
    ch

---
+++ Homework08-Delegates-Events/01.InterestCalculator/InterestCalculatorExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InterestCalculator
+{
+    public class InterestCalculatorExample : InterestCalculator
+    {
+        public static void Main()
+        {
+            InterestCalculator compountInterest = new InterestCalculator
+                (500, 5.6m, 10, InterestCalculator.GetCompoundInterest);
+            Console.WriteLine("{0:F4}", compountInterest.CalcualtedInterest);
+
+            InterestCalculator simpleInterest = new InterestCalculator(2500, 7.2m, 15, GetSimpleInterest);
+            Console.WriteLine("{0:F4}", simpleInterest.CalcualtedInterest);
+        }
+    }
+}
